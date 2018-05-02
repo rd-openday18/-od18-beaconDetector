be951b31b7f2a7fa801f09e665257e2edfd126d3
btrinite <btrinite@free.fr>
2018-05-02 12:09:28 +0200

    rework and stats

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
 # Beacon Detector
-    This simple program detects and reports continuously Bleutooth Low Energy (BLE) advertisements to Google PubSub topic.
-    It also reports itself every 5 sec (as a beacon gateway) to another topics in order to have a minimalistic monitoring solution of the detector itself. 
-    (BLE Advertisement are usually sent by Beacon/Tag but also by some smartphones/headset/...)
+This simple program detects and reports continuously Bleutooth Low Energy (BLE) advertisements to Google PubSub topic.
+It also reports itself every 5 sec (as a beacon gateway) to another topics in order to have a minimalistic monitoring solution of the detector itself.
+(BLE Advertisement are usually sent by Beacon/Tag but also by some smartphones/headset/...)
 
-    This program do not use full Google Cloud Platform SDK because of targeted platform (a Raspberry PI Zero/arm6)
-    Google Authentication lib is used then raw REST API call are used. 
+This program do not use full Google Cloud Platform SDK because of targeted platform (a Raspberry PI Zero/arm6)
+Google Authentication lib is used then raw REST API call are used.
 
-    WIP
+WIP
 
 # Installation
-```sh 
+```sh
 npm install
 ```
+
 # usage
-    this program is intend to be used with google PubSub. You will need to setup a service account and download the service account json key file then setup GOOGLE_APPLICATION_CREDENTIALS accordingly. [Check Google page on authentication](https://cloud.google.com/docs/authentication/)
+this program is intend to be used with google PubSub. You will need to setup a service account and download the service account json key file then setup GOOGLE_APPLICATION_CREDENTIALS accordingly. [Check Google page on authentication](https://cloud.google.com/docs/authentication/)
+Other env variable to consider :
+GW_PUBLISH_PERIOD : period to publish gw status (in ms)
+BEACON_DISCOVERY_TOPIC_NAME : google pubsub topic name to use to publish beacon advertisements
+GW_DISCOVERY_TOPIC_NAME : google pubsub topic name to use to publish gateway status
+CONTINUOUS_SCAN : scan mode:
+    true : continuous scan (each BLE advertisement received is published instantaneously),
+    false: batch mode : gateway publishes detected beacons only once per beacon in a given recurrent period
+SCAN_PERIOD : scan period when working in batch mode (in ms)
+
 
 ```sh
 export GOOGLE_APPLICATION_CREDENTIALS=<your gcp json file>
--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Few params
 const GWPingPeriod = 5000;
 
+// Env in Dev
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
 //dependencies
 var noble = require('noble');
 const {auth} = require('google-auth-library');
@@ -10,11 +15,18 @@ var ifaces = os.networkInterfaces();
 
 // GLobal var
 var detectoruuid = '';
-var inet = [];
+var stats = {
+  maxpending : 0,
+  published_success : 0,
+  published_failure : 0,
+  'window': {
+    period: process.env.GW_PUBLISH_PERIOD,
+    maxpending : 0,
+    published_success : 0,
+    published_failure : 0  
+  }
+};
 
-// Const
-const beaconTopicName = 'beaconping';
-const gwTopicName = 'gwping';
 
 // Google stuff
 var googleClient = null;
@@ -23,16 +35,16 @@ var gwPublishUrl = null;
 async function initGooglePubSub () {
     const projectId = await auth.getDefaultProjectId();
     const baseUrlPublish = `https://pubsub.googleapis.com/v1/projects/${projectId}/topics`;
-    beaconPublishUrl = `${baseUrlPublish}/${beaconTopicName}:publish`
-    gwPublishUrl = `${baseUrlPublish}/${gwTopicName}:publish` 
-    console.log ("Google PubSub : URL init Done")   
+    beaconPublishUrl = `${baseUrlPublish}/${process.env.BEACON_DISCOVERY_TOPIC_NAME}:publish`
+    gwPublishUrl = `${baseUrlPublish}/${process.env.GW_DISCOVERY_TOPIC_NAME}:publish`
+    console.log ("Google PubSub : URL init Done")
 }
 
 async function googleAuthenticate() {
     googleClient = await auth.getClient({
       scopes: 'https://www.googleapis.com/auth/pubsub'
     });
-    console.log ("Google PubSub : Authentication Done")   
+    console.log ("Google PubSub : Authentication Done")
 }
 
 async function gcpPublish (payload) {
@@ -43,25 +55,42 @@ async function gcpPublish (payload) {
 var googlePublishQueue = async.queue(function (task, callback) {
     try {
         gcpPublish(task.payload).then(function(rest) {
+            stats.published_success++;
+            stats.window.published_success++;
             if (callback!=undefined) {callback({status:true, result:rest})};
         });
     } catch (e) {
+        stats.published_failure++;
+        stats.window.published_failure++;
         console.log (e);
         if (callback!=undefined) {callback({status:true, result:e})};
-    } 
+    }
 }, 2);
 
 googlePublishQueue.drain = function() {
-    console.log('All beacons AD have been published');
+    //console.log('All beacons AD have been published');
+}
+
+googlePublishQueue.saturated = function() {
+    //console.log('Worker queue saturated '+googlePublishQueue.length());
+    stats.maxpending=Math.max(stats.maxpending, googlePublishQueue.length());
+    stats.window.maxpending=Math.max(stats.window.maxpending, googlePublishQueue.length());
+}
+
+function resetWindowedStats ()
+{
+    stats.window.maxpending=0;
+    stats.window.published_success=0;
+    stats.window.published_failure=0;
 }
 
 function beaconPublished (err) {
     if (err.status == false) {
         console.log ("Google pubsub return error "+err.result)
     } else {
-        console.log ("Beacon published "+JSON.stringify (err.result.data))
+        // console.log ("Beacon published "+JSON.stringify (err.result.data))
     }
-} 
+}
  async function BLEDiscovered (peripheral) {
     peripheral.ts = (new Date).getTime();
     var beaconPing={ 'ts':peripheral.ts.toString(), 'gwid':detectoruuid, 'address':peripheral.address, 'rssi':peripheral.rssi.toString(), 'name':((peripheral.advertisement.localName != undefined) ? peripheral.advertisement.localName: "uknown"), 'txpower':((peripheral.advertisement.txPowerLevel != undefined) ? peripheral.advertisement.txPowerLevel.toString():'unknown')}
@@ -70,9 +99,9 @@ function beaconPublished (err) {
 }
 
 async function BLEScanSignatures() {
-    console.log ("Start Beacon Detection")
-    noble.stopScanning();    
-    noble.startScanning([], true);    
+    //console.log ("Start Beacon Detection")
+    noble.stopScanning();
+    noble.startScanning([], (process.env.CONTINUOUS_SCAN==true)?true:false);
 }
 
 BLEState = function (state) {
@@ -80,26 +109,35 @@ BLEState = function (state) {
     switch(state) {
         case 'poweredOn':
             initGooglePubSub();
-            googleAuthenticate()    
+            googleAuthenticate()
+            if (process.env.CONTINUOUS_SCAN == 'false') {
+                // Scan by batch, repeat scan procedure each period
+                console.log (`Mode : Batch (${process.env.SCAN_PERIOD})`)
+                setInterval(BLEScanSignatures, process.env.SCAN_PERIOD)
+            } else {
+                // continuous scan mode
+                console.log ("Mode : Continuous")
+            }
+            // At least, start scan one(first) time soon.
             setTimeout (BLEScanSignatures,1000);
             break;
         default:
+            noble.stopScanning();
         break;
     }
 }
 
  async function GWPing () {
-
     inet = [];
     Object.keys(ifaces).forEach(function (ifname) {
         var alias = 0;
-        
+
         ifaces[ifname].forEach(function (iface) {
             if ('IPv4' !== iface.family || iface.internal !== false) {
             // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
             return;
             }
-        
+
             if (alias >= 1) {
             } else {
             // this interface has only one ipv4 adress
@@ -110,24 +148,53 @@ BLEState = function (state) {
     });
 
 
-    gwping={'ts':((new Date).getTime()).toString(), 'gwid':detectoruuid, 'ip':inet[0].ip};
+    gwping={'ts':((new Date).getTime()).toString(), 'gwid':detectoruuid, 'ip':inet[0].ip, 'stats':JSON.stringify(stats)};
+    resetWindowedStats();
+    //console.log (gwping)
     try {
         var payload=Buffer.from(JSON.stringify(gwping)).toString('base64')
         const res = await googleClient.request({ method: 'post', url:gwPublishUrl, data:{ messages: [ { data: payload} ] } });
-        console.log ("GW ID published "+JSON.stringify (res.data))
+        //console.log ("GW ID published "+JSON.stringify (res.data))
     } catch (e) {
         console.error(e);
     }        //console.log (JSON.stringify(gwpingPOSTargs.data))
-    setTimeout(GWPing, GWPingPeriod)    
 }
 
-noble.on('discover', BLEDiscovered);
-noble.on('stateChange', BLEState);
+function checkConfig(callback) {
+    if (process.env.BEACON_DISCOVERY_TOPIC_NAME == undefined) {
+        callback (false, "BEACON_DISCOVERY_TOPIC_NAME is missing")
+    } else
+    if (process.env.GW_DISCOVERY_TOPIC_NAME == undefined) {
+        callback (false, "GW_DISCOVERY_TOPIC_NAME is missing")
+    } else
+    if (process.env.GW_PUBLISH_PERIOD == undefined) {
+        callback (false, "GW_PUBLISH_PERIOD is missing")
+    } else
+    if (process.env.CONTINUOUS_SCAN == undefined) {
+        callback (false, "CONTINUOUS_SCAN is missing")
+    } else
+    if ((process.env.CONTINUOUS_SCAN == 'false') && (process.env.SCAN_PERIOD == undefined)) {
+        callback (false, "SCAN_PERIOD is missing")
+    } else {
+        callback (true, "Let's go");
+    }
+}
 
-require("machine-uuid")(function(uuid) {
-    detectoruuid = uuid;
-    console.log ("BLE Detector unique ID: "+detectoruuid)
-    setTimeout (GWPing, GWPingPeriod);
+checkConfig(function (res, msg) {
+    if (res == true) {
+        // let'g get platform UUID
+        require("machine-uuid")(function(uuid) {
+            detectoruuid = uuid;
+            console.log ("BLE Detector unique ID: "+detectoruuid)
+            // Start regular publish of gw identity
+            setInterval (GWPing, process.env.GW_PUBLISH_PERIOD);
+            // start lstening beacons
+            noble.on('discover', BLEDiscovered);
+            noble.on('stateChange', BLEState);
+        })
+    } else {
+        console.log ("Error while checking mandatory params: "+msg)
+        process.exit(1);
+    }
 })
-
 //noble.startScanning();
