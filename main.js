13ba58300d1045b5d7777e7dc23051f99df3e459
btrinite <btrinite@free.fr>
2018-05-04 15:10:30 +0200

    bug stats

--- main.js
+++ main.js
@@ -180,7 +180,7 @@ BLEState = function (state) {
     });
 
 
-    gwping={'ts':((new Date).getTime()).toString(), 'gwid':detectoruuid, 'ip':inet[0].ip, 'stats':stats};
+    gwping={'ts':((new Date).getTime()).toString(), 'gwid':detectoruuid, 'ip':inet[0].ip, 'stats':lastStats};
     resetWindowedStats();
     //console.log (gwping)
     try {
